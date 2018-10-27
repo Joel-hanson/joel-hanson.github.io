2a68975a83d94d099cf0a7af65b315396434e5c5
Joel-hanson <joelhanson025@gmail.com>
2018-10-27 22:08:10 +0530

    dont know what i am doing

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import TypeWriter from './components/typewriter.js';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    },
+    components: {
+        TypeWriter
+    }
+});
+
