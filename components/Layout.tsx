81f0057117fb7f9667a4b08e4cf639ff43faabe9
Joel Hanson <joelhanson025@gmail.com>
2021-03-08 08:54:04 +0530

    The favicon for the website

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -99,6 +99,13 @@ const Layout: React.FunctionComponent<LayoutInterface> = ({
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+        <meta name="msapplication-TileColor" content="#ef5350" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Header items={navItems}></Header>
       {children}
